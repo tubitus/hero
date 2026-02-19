0c6442142e5a056281c750acb98d50dc7b87bc53
Jakub Vlček <Jakub.Vlcek@student.souepl.cz>
2026-02-19 13:16:09 +0100

    pomoc pls

--- script/script.js
+++ script/script.js
@@ -1,7 +1,15 @@
 const hamburger =   document.querySelector(".hamburger");
 const nav = document.querySelector("nav");
+const navLinks = nav.querySelectorAll("a");
 
 hamburger.addEventListener("click", () => {
     nav.classList.toggle("active");
     hamburger.classList.toggle("active");
+});
+
+navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        nav.classList.remove("active");
+        hamburger.classList.remove("active");
+    });
 });
